f4e3dc37b89ecc9f77bd18ac295cb95b7d1046aa
Gabriel <gfreivasc@gmail.com>
2016-01-20 13:03:59 -0300

    Heroku db settings

--- shortenme/settings.py
+++ shortenme/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
 }
 
 # Parse database configuration from $DATABASE_URL
-# DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config()
 
 # Enable Persistent Connections
 DATABASES['default']['CONN_MAX_AGE'] = 500
